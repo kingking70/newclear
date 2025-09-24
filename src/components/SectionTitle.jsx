62ba6ba5173b6a2210eec130bb357b2cbdfbb554
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-09-24 12:15:02 +0800

    Fix font path for Text3D component

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import { Text3D, Center } from "@react-three/drei";
 export const SectionTitle = ({ children, ...props }) => {
   return (
     <Center center>
-      <Text3D font='../../public/fonts/Inter_Bold.json' size={0.5} {...props}>
+      <Text3D font='/fonts/Inter_Bold.json' size={0.5} {...props}>
       {children}
         <meshStandardMaterial color="white" />
       </Text3D>
