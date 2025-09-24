bf9740dd1a44d5f07a47494224e0da2ad943a730
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-09-24 15:00:58 +0800

    solved? egg issue

--- src/components/models/Egg.jsx
+++ src/components/models/Egg.jsx
@@ -1,9 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Egg(props) {
-    // Use a Vite-friendly URL so the asset is bundled and paths are rewritten in build
-    const url = new URL("../../../assets/egg/scene.gltf", import.meta.url).href
-    const { scene } = useGLTF(url)
+    const { scene } = useGLTF('egg/scene.gltf')
  
     return <primitive object={scene} {...props} />
   }
