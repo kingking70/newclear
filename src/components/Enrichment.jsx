976941c1c0f8d00cfdd0b49c56117cf9d77c58b0
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-12-12 13:08:20 +0800

    fixed bug: removed broken supabase link/asset

--- src/components/Enrichment.jsx
+++ src/components/Enrichment.jsx
@@ -14,9 +14,7 @@ export default function Enrichment(props) {
                 <group scale={0.2}>
                     { /* to check how to make maxWidth responsive */}
                     <Text position={[-1, 15, 2]} maxWidth={20}>
-                        This is the process where the isotopic proportion of U-235 is increased from 0.72% to 94%. 
-                        If U-235 isotopic proportion remains below 20%, it is considered low enriched. Most commercial reactors use low enriched uranium (LEU) below 5% as fuel aka "reactor grade uranium". LEU does not deterioriate and can be safely stored for many years.
-                        If U-235 isotopic proportion enriches beyond 20%, it is considered highly enriched. These uraniums are mostly used in naval propulsion reactors e.g. submarines, nuclear weapons, and some research reactors.    
+                         
                         <meshStandardMaterial color={"black"}/>
                     </Text>
                     <Text position={[-1, 17, 2]} maxWidth={23}>
--- src/components/Mining&Milling.jsx
+++ src/components/Mining&Milling.jsx
@@ -38,7 +38,7 @@ export default function MiningAndMilling(props) {
                 
                 <group ref={UraniumRef}>
                     <Float >
-                        <Uranium scale={0.2} color="#ffd60a" position={[-2, 0, 1]}/>
+                        
                     </Float>
                 </group>
 
@@ -52,14 +52,13 @@ export default function MiningAndMilling(props) {
                 <group scale={0.2}>
                     { /* to check how to make maxWidth responsive */}
                     <Text position={[0, 10, 2]} maxWidth={25}>
-                        A chicken egg sized uranium fuel can provide as much electricity as 88 tonnes of coal!!
-                        according to the International Atomic Energy Agency (IAEA). Uranium is the one of the most common elements on earth, about 500x more common than gold. It is present in rock, soil, water, and even our bodies. There are approximately 4 billion tonnes of highly diluted uranium in the ocean. The 3 naturally occuring isotopes, same element and chemical properties but in different variations such as mass and physical properties, are uranium-234(U-234), U-235, and lastly, which makes up for 99% out of the 3 uraniums, U-238. Most nuclear reactors use U-235. However, natural uranium typically contains only 0.72% of U-235 and most reactors need a higher concentration of this isotope for fuel. Therefore, an artificial enrichment process is used to increase U-235 concentration. Only in Canada, its reactors called Canada Deuterium Uranium (CANDU) reactors are fuelled with non-enriched uranium. 
+                         
                         
                         
                         <meshStandardMaterial color={"black"}/>
                     </Text>
                     <Text position={[-1, -10, 2]} maxWidth={23}>
-                        I'll reference some of my information from the book, rad future by isabelle boemeke. uranium mining is now, oct 2025, one of the safest and most tightly regualated in the world. according to hannah ritchie, data scientist and researcher, the amount of mining needed for clean energy for the whole world is 500 to 1000 times less than the current amount we are mining for fossil fuels. the Breakthrough institute reports, '1 gigawatt-hour of electricity with nuclear requires 30% of the rocks and metals needed for solar and just 23% for onshore winds'. more than 50% of uranium mining uses insitu recovery, a process which skips the need for open pits or tunnels. a liquid, made of water, oxygen, hyroden peroxide, and either soduium carbonate or carbon dioxide, is injected into the ground to dissolve uranium which is then pumped to the surface. what happens after?   
+                         
                         <meshStandardMaterial color={"black"}/>
                     </Text>
                 </group>
